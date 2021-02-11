828721f65919d16200b7628ff0be5b9c5ade165a
Logan Zehr <zehrl315@outlook.com>
2021-02-11 10:53:28 -0800

    get /api/workouts works now. Server.js was referencing the wrong database. Changed database to workout.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,8 @@ const router = require("express").Router();
 // MongoDB Model(s)
 const Workout = require("../models/workout");
 
-router.get("/api/workouts", ({ body }, res) => {
-    Workout.find({})
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
         .then(data => {
             res.json(data);
         })
@@ -14,37 +14,37 @@ router.get("/api/workouts", ({ body }, res) => {
     // res.send("GET /api/workouts");
 });
 
-router.put("/api/workouts/:id", ({ body }, res) => {
-    Workout.create(body)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    // res.send("PUT /api/workouts");
-});
+// router.put("/api/workouts/:id", ({ body }, res) => {
+//     Workout.create(body)
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+//     // res.send("PUT /api/workouts");
+// });
 
-router.post("/api/workouts", ({body}, res) => {
-    Workout.create(body)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+// router.post("/api/workouts", ({body}, res) => {
+//     Workout.create(body)
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
 
-router.get("/api/workouts/range", (req, res) => {
-    //   Transaction.find({})
-    //     .sort({ date: -1 })
-    //     .then(dbTransaction => {
-    //       res.json(dbTransaction);
-    //     })
-    //     .catch(err => {
-    //       res.status(400).json(err);
-    //     });
-    res.send("GET /api/workouts/range");
-});
+// router.get("/api/workouts/range", (req, res) => {
+//     //   Transaction.find({})
+//     //     .sort({ date: -1 })
+//     //     .then(dbTransaction => {
+//     //       res.json(dbTransaction);
+//     //     })
+//     //     .catch(err => {
+//     //       res.status(400).json(err);
+//     //     });
+//     res.send("GET /api/workouts/range");
+// });
 
 module.exports = router;
