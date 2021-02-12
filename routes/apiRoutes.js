6e22036ca943c516a185b457dd7b35382cfa0544
Logan Zehr <zehrl315@outlook.com>
2021-02-11 16:17:50 -0800

    post /api/workouts works correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,36 +4,40 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
+    Workout.find()
         .then(data => {
             res.json(data);
         })
         .catch(err => {
             res.status(400).json(err);
         });
-    // res.send("GET /api/workouts");
 });
 
-// router.put("/api/workouts/:id", ({ body }, res) => {
-//     Workout.create(body)
-//         .then(data => {
-//             res.json(data);
-//         })
-//         .catch(err => {
-//             res.status(400).json(err);
-//         });
-//     // res.send("PUT /api/workouts");
-// });
 
-// router.post("/api/workouts", ({body}, res) => {
-//     Workout.create(body)
-//         .then(data => {
-//             res.json(data);
-//         })
-//         .catch(err => {
-//             res.status(400).json(err);
-//         });
-// });
+// Postman is setup
+// Route will send data correctly, but i'm not sure if i need the exercise id in order to update the document
+router.put("/api/workouts/:id", ({ params: { id }, body }, res) => {
+
+    Workout.findByIdAndUpdate({ id, body })
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    // res.send("PUT /api/workouts");
+});
+
+// Create new workout
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 // router.get("/api/workouts/range", (req, res) => {
 //     //   Transaction.find({})
