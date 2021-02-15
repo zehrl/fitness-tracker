47b9c18c789d01f015de802e2bb932b0447cb8d3
Logan Zehr <zehrl315@outlook.com>
2021-02-15 10:53:57 -0800

    Heroku deployment variable

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ app.use(express.json());
 // Define public folder
 app.use(express.static("public"));
 
-// Mongoose DB Setup
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Define routes folder
 app.use(require("./routes/apiRoutes"));
